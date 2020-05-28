c2e4747b6ad702f8373e74599b464adbd99919f3
Jeff Knupp <jeff@intelligent-artifacts.com>
2020-05-28 11:10:33 -0400

    Remove requirements building code

--- setup.py
+++ setup.py
@@ -19,10 +19,6 @@ with open('Version', encoding='utf-8') as f:
 with open('README.rst', encoding='utf-8') as f:
     readme = f.read()
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-requirements = parse_requirements('requirements.txt', session=False)
-requirements = [str(ir.req) for ir in requirements]
-
 __NAME__ = 'Flask-MailGun3'
 __doc__ = readme
 __author__ = 'Amey-SAM'
